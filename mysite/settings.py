35a5477108e81565ebc86abf5bd32ca844b83429
popeashiedu <popeashiedu@gmail.com>
2020-07-15 14:58:51 +0100

    trial 13

--- mysite/settings.py
+++ mysite/settings.py
@@ -132,11 +132,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles')
+    os.path.join(BASE_DIR, 'static')
     ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # E-mail
 # if not DEBUG:
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
